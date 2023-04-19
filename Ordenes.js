419704d033d942a2c028d4b5fa00056fab22a7fc
Luz <luz441rovira@gmail.com>
2023-04-18 23:40:30 -0600

    PageV002

---
+++ Ordenes.js
@@ -0,0 +1,21 @@
+// function redireccion(){
+// let tipo = document.getElementById(userType).value
+
+//     if (document.getElementById(userType) = "Desarrollador") {
+//         window.location.href = "desarrollador.html"
+//     } else if (tipo = "Pug") {
+//         location.href = "pug.html"
+//     } else if (tipo = "Chewbaca") {
+//         location.href = "Chewbaca.html"
+//     } else if (tipo = "RataWomp") {
+//         location.href = "pug.html"
+//     } else {
+//         console.log(NoData)
+//     }
+// }
+
+let boton = document.querySelector(boton);
+
+boton.addEventListener('click', function(){
+    window.location.href = 'http://google.com.mx';
+})
